0b0ec81e1facb5d8c4f211e2c24d7bd320d0eac3
danielemontesi <daniele.montesi@gmail.com>
2016-09-19 12:40:23 +0200

    .

--- src/Iubar/Slim/Core/AbstractController.php
+++ src/Iubar/Slim/Core/AbstractController.php
@@ -23,6 +23,12 @@ abstract class AbstractController {
 	 */
 	public function encodeHTML($str){
 		return htmlentities($str, ENT_QUOTES, 'UTF-8');
+			// NOTA:
+		// Urlencode() is used to encode strings for urls.
+		// HTMLentities() is a way to encode a HTML string, so that when displayed in a file it's not parsed by the browser.
+		// So I use htmlspecialchars (or htmlentities) to encode strings in HTML.
+		//
+		// And if this URL is placed in a HTML attribute (Such as the action attribute of a form element), you should further encode the URL with htmlspecialchars
 	}
 
 }
