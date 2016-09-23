138aa4d800eeb9abebd15f4e087bc65654251012
danielemontesi <daniele.montesi@gmail.com>
2016-09-23 12:19:46 +0200

    .

--- src/Iubar/Slim/Core/JsonSafeAbstractController.php
+++ src/Iubar/Slim/Core/JsonSafeAbstractController.php
@@ -15,11 +15,20 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
 
     abstract protected function getApikey($user);
 
+    private $user = null;
+    private $ts_str = null;
+    private $hash = null;
+
     public function __construct(){
         parent::__construct();
     }
 
     protected function isAuthenticated(){
+        $request = $this->app->request;
+        $this->user = $request->params('user');
+        $this->ts_str = rawurldecode($request->params('ts'));
+        $this->hash = $request->params('hash');
+
         $b = $this->validate();
         if(!$b){
             $this->sendHmacError();
@@ -27,74 +36,43 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
         return $b;
     }
 
-    private function sendHmacError($debug = false){
-        $request = $this->app->request;
-        $user = $request->params('user');
-        $ts_str = rawurldecode($request->params('ts'));
-        $hash = $request->params('hash');
-        $allPostVars = $this->app->request->post();
+    private function sendHmacError(){
 
-        $ts = $this->parseTimestamp($ts_str);
-
-        if(!$debug){
-
-            $error = 'Erroe sconosciuto';
+        $ts = $this->parseTimestamp($this->ts_str);
+        $error = 'Erroe sconosciuto';
+        $code = ResponseCode::BAD_REQUEST;
+        if(!$this->user || !$this->ts_str || !$this->hash){
+            $error = "Message authentication failed: Wrong message format !";
+            $error .= "\ts_str = $this->ts_str";
+            $error .= "\user = $this->user";
+            $error .= "\hash = $this->hash";
             $code = ResponseCode::BAD_REQUEST;
-            if(!$user || !$ts_str || !$hash){
-                $error = "Message authentication failed: Wrong message format !";
-                $error .= "\$ts_str = $ts_str";
-                $error .= "\$user = $user";
-                $error .= "\$hash = $hash";
-                $error .= "\$allPostVars = " . json_encode($allPostVars);
-                $code = ResponseCode::BAD_REQUEST;
-            }else if(!$ts){
-                $error = "Message authentication failed: Wrong timestamp format!";
-                $code = ResponseCode::BAD_REQUEST;
-            }else if(!self::isTimeStampValid($ts)){
-                $error = "Message authentication failed: Time offeset is too big !";
-                $code = ResponseCode::UNAUTHORIZED;
-            }else{
-                $api_key = $this->getApikey($user);
-                // $new_api_key = \Application\Utils\ApiKeyUtils::create();
-                $expected_hash = $this->calcHash($user, $api_key, $ts_str); // WARNING: security breach !!!
-
-                if($expected_hash != $hash){
-                    $error = "Message authentication failed: Wrong hash !";
-                    $code = ResponseCode::UNAUTHORIZED;
-                }
-            }
-            $this->responseStatus($code, array(), $error);
-
+        }else if(!$ts){
+            $error = "Message authentication failed: Wrong timestamp format!";
+            $code = ResponseCode::BAD_REQUEST;
+        }else if(!self::isTimeStampValid($ts)){
+            $error = "Message authentication failed: Time offeset is too big !";
+            $code = ResponseCode::UNAUTHORIZED;
         }else{
-
-            // Questo blocco di codice è utile solo al fine di deguggare il metodo
-
-            $ts_reparsed = "error";
-            if($ts){
-                $ts_reparsed = $ts->format(\DateTime::RFC3339); // Verifico se il parsing del timestamp è stato effettuato correttamente
-            }
-            $api_key = $this->getApikey($user);
+            $api_key = $this->getApikey($this->user);
             // $new_api_key = \Application\Utils\ApiKeyUtils::create();
-            $expected_hash = $this->calcHash($user, $api_key, $ts_str); // WARNING: security breach !!!
+            $expected_hash = $this->calcHash(); // WARNING: security breach !!!
 
-            $debug_data = array(
-                'user'          =>  $user,
-                'ts'            =>  rawurlencode($ts_str), // rawurlencode() è obbligatorio perchè la stringa contiene il segno "+"
-                'hash'          =>  $hash,
-                'hash_expected' =>  $expected_hash,
-                // 'new_api_key'=>  $this->formatApiKey($new_api_key),
-                'ts_reparsed'   =>  rawurlencode($ts_reparsed)
-
-            );
-            $this->responseStatus(ResponseCode::UNAUTHORIZED, json_encode($debug_data), "Errore");
+            if($expected_hash != $this->hash){
+                $error = "Message authentication failed: Wrong hash !";
+                $code = ResponseCode::UNAUTHORIZED;
+            }
         }
+        $this->responseStatus($code, array(), $error);
+
     }
 
-    private function calcHash($user, $api_key, $ts_str){
+    private function calcHash(){
         $hash = null;
-        if($user && $ts_str && $api_key){
-            $url = 'http://' . $_SERVER['HTTP_HOST'] . $this->app->request->getRootUri() . $this->app->request->getResourceUri();
-            $data = $url . $user . $ts_str . $api_key;
+        $request = $this->app->request;
+        if($this->user && $this->ts_str && $api_key){
+            $url = 'http://' . $_SERVER['HTTP_HOST'] . $request->getRootUri() . $request->getResourceUri();
+            $data = $url . $this->user . $this->ts_str . $api_key;
             $raw_hash = hash_hmac('sha256', $data, $api_key, true); // Vedi https://en.wikipedia.org/wiki/Hash-based_message_authentication_code
             $hash = base64_encode($raw_hash);
         }
@@ -102,7 +80,6 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
     }
 
     protected function validate(){
-        $user = $this->app->request->params('user');
 
         // From the comments in the PHP manual page (php.net/manual/en/function.urlencode.php) :
         // "Don't use urlencode() or urldecode() if the text includes an email address,
@@ -111,33 +88,28 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
         // AND you need the readability provided by the non-standard "+" usage,
         // instead always use use rawurlencode() or rawurldecode().
 
-        // Nota: il formato data RFC3339 presenta il segno "+" nella descrizione della tiemzone
-        // per tale motivo non utilizzo urlencode() / urldecode()
-
-        $ts_str = rawurldecode($this->app->request->params('ts'));
-        $hash = $this->app->request->params('hash');
-        if(!$user || !$ts_str || !$hash){
+        if(!$this->user || !$this->ts_str || !$this->hash){
             return false;
         }
          
-        $api_key = $this->getApikey($user);
+        $api_key = $this->getApikey($this->user);
         if(!$api_key){
             return false;
         }
          
-        $ts = $this->parseTimestamp($ts_str);
+        $ts = $this->parseTimestamp($this->ts_str);
         if(!$ts || !self::isTimeStampValid($ts)){
             return false;
         }
 
-        $expected = $this->calcHash($user, $api_key, $ts_str);
+        $expected = $this->calcHash();
         if(!$expected){
             return false;
         }
 
          
 
-        if(\Iubar\Misc\Encryption::hashEquals($expected, $hash)){ // hash_equals($expected, $hash); // Solo per PHP > 5.6
+        if(\Iubar\Misc\Encryption::hashEquals($expected, $this->hash)){ // hash_equals($expected, $this->hash); // Solo per PHP > 5.6
             return true;
         }else{
             return false;
@@ -158,7 +130,7 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
 
 
 
-    private static function isTimeStampValid(\DateTime $dateTime, $max_sec=1200){ // default time-window is 20 minutes
+    private static function isTimeStampValid(\DateTime $dateTime, $max_sec=1200){ // here default time-window is 20 minutes (1200 seconds)
         // Note: a common issue when using time-windows for request,
         // is that either server or client is not using the correct time.
         // Make sure both server and client are using systems like NTP to keep times in sync
@@ -182,6 +154,8 @@ abstract class JsonSafeAbstractController extends JsonAbstractController {
 
     private static function getTimeStampString(){
         $now = new \DateTime();
+        // Nota: il formato data RFC3339 presenta il segno "+" nella descrizione della tiemzone
+        // per tale motivo non utilizzo urlencode() / urldecode()
         $str = $now->format(\DateTime::RFC3339);
         return $str;
     }
