2cb0ce0e32ff88f14753a387f75cfca247f1c118
daniele <daniele.montesi@iubar.it>
2017-12-22 11:18:15 +0100

    .

---
+++ src/Iubar/Slim/Core/ApiJwtController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Iubar\Slim\Core;
+
+use Iubar\Slim\Core\JsonAbstractController;
+use Iubar\Slim\Core\ResponseCode;
+use Iubar\Misc\Encryption;
+use Firebase\JWT\JWT;
+/**
+ *
+ * @author Daniele
+ *
+ * HMAC security
+ */
+abstract class ApiJwtController extends JsonAbstractController {
+
+	abstract protected function getApikey($user_id);
+
+	private $algorithm = 'HS512';
+
+	public function __construct(){
+		parent::__construct();
+	}
+
+	protected function isAuthenticated(){
+
+			$jwt =  $request->params('jwt');
+
+			 if ($jwt) {
+			 	try {
+
+			 		/*
+			 		 * decode the jwt using the key from config
+			 		 */
+			 		$secretKey = $this->getApikey($user_id);
+
+			 		/**
+			 		 * You can add a leeway to account for when there is a clock skew times between
+			 		 * the signing and verifying servers. It is recommended that this leeway should
+			 		 * not be bigger than a few minutes.
+			 		 *
+			 		 * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+			 		 */
+			 		JWT::$leeway = 60; // $leeway in seconds
+
+			 		$token = JWT::decode($jwt, $secretKey, array($this->algorithm));
+			 		print_r($decoded);
+			 		/*
+			 		 NOTE: This will now be an object instead of an associative array. To get
+			 		 an associative array, you will need to cast it as such:
+			 		 */
+			 		$decoded_array = (array) $decoded;
+			 		if(!empty($decoded_array)){
+			 			return true;
+			 		}
+
+			 	} catch (\Exception $e) {
+			 		/*
+			 		 * the token was not able to be decoded.
+			 		 * this is likely because the signature was not able to be verified (tampered token)
+			 		 */
+			 		header('HTTP/1.0 401 Unauthorized');
+			 	}
+			 } else {
+			 	/*
+			 	 * The request lacks the authorization token
+			 	 */
+			 	header('HTTP/1.0 400 Bad Request');
+			 	echo 'Token not found in request';
+			 }
+ 			return false;
+
+	}
+
+	public function buildJwtToken($user_id){
+		$jwt = $this->createToken($user_id);
+		$unencodedArray = ['jwt' => $jwt];
+		$jwt_as_json = json_encode($unencodedArray);
+		return $jwt_as_json;
+	}
+
+	private function createToken($user_id){
+
+		$tokenId    = base64_encode(mcrypt_create_iv(32));
+		$issuedAt   = time();
+		$notBefore  = $issuedAt + 10;  //Adding 10 seconds
+		$expire     = $notBefore + 60; // Adding 60 seconds
+		$serverName = $_SERVER['HTTP_HOST'];
+
+		/*
+		 * Create the token as an array
+		 * https://tools.ietf.org/html/rfc7519#section-4.1.1
+		 */
+		$data = [
+			'iss'  => $serverName,       // Issuer
+			'iat'  => $issuedAt,         // Issued at: time when the token was generated
+			'nbf'  => $notBefore,        // Not before
+			'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
+			'exp'  => $expire,           // Expire
+
+			/*
+			 // Example: https://github.com/settings/tokens (see TravisCI)
+			 'scopes' => {
+			 'users' => {
+			 'actions' => ['read', 'create']
+			 },
+			 'users_app_metadata' => {
+			 'actions' => ['read', 'create']
+			 }
+			 },
+			 */
+
+			'data' => [                  // Data related to the signer user
+				// 'userId'   => $rs['id'], // userid from the users table
+				// 'userName' => $username, // User name
+				'userId' => $user_id
+			]
+		];
+
+		/*
+		 * Extract the key, which is coming from the config file.
+		 *
+		 * Best suggestion is the key to be a binary string and
+		 * store it in encoded in a config file.
+		 *
+		 * Can be generated with base64_encode(openssl_random_pseudo_bytes(64));
+		 *
+		 * keep it secure! You'll need the exact key to verify the
+		 * token later.
+		 */
+		$secretKey = $this->getApikey($user_id);
+
+		/*
+		 * Encode the array to a JWT string.
+		 * Second parameter is the key to encode the token.
+		 *
+		 * The output string can be validated at http://jwt.io/
+		 */
+		$jwt = JWT::encode(
+			$data,      //Data to be encoded in the JWT
+			$secretKey, // The signing key
+			$this->algorithm  // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
+			);
+	}
+
+	protected function decodeJwtToken($jwt){
+		$secretKey = $this->getApikey($user_id);
+		JWT::$leeway = 60; // $leeway in seconds
+		$token = JWT::decode($jwt, $secretKey, array($this->algorithm));
+		if (!$this->isJwtArrayValid($token)){
+			throw new \LogicException('Wrong Jwt array'); // TODO: meglio restituire errore 401 Unauthorized
+		}
+		return $token;
+	}
+
+	protected function getJwtToken($email{
+		$token = null;
+		$api_key = $this->getApikey($email);
+		if ($this->isUserValid($email, $api_key)){
+			$token = $this->createToken($email);
+		} else {
+			throw new \InvalidArgumentException('Email or api key wrong');
+		}
+
+		return $token;
+	}
+
+	private function isJwtArrayValid(array $data){
+		$b = false;
+		if (isset($data['data'])){
+			$email = $data['data']->user;
+			if ($email !== null){
+				$b = $this->isUserRegistered($email);
+			}
+		}
+
+		return $b;
+	}
+
+
+	private function isUserValid($email, $api_key){
+		return $this->isUserRegistered($email);
+	}
+
+	private function isUserRegistered($email){
+		if(!$this->getApikey($email)){
+			return false
+		}
+		return true;
+	}
+
+}
---
+++ src/Iubar/Slim/Core/JsonSimpleSafeAbstractController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Iubar\Slim\Core;
+
+use Iubar\Slim\Core\JsonAbstractController;
+use Iubar\Slim\Core\ResponseCode;
+use Iubar\Misc\Encryption;
+/**
+ *
+ * @author Daniele
+ *
+ * HMAC security
+ */
+abstract class JsonSimpleSafeAbstractController extends JsonAbstractController {
+
+	abstract protected function getApikey($user_id);
+
+	protected $user = null;
+	protected $ts_str = null;
+	protected $hash = null;
+
+	public function __construct(){
+		parent::__construct();
+	}
+
+	protected function isAuthenticated(){
+		$request = $this->app->request;
+		$this->user = $request->params('user');
+		// E' inutile invocare rawurldecode(), poichè  $request->params() effettua già la stessa decodifica
+		// $this->ts_str = rawurldecode($request->params('ts'));
+		$this->ts_str = $request->params('ts');
+		$this->hash = $request->params('hash');
+		$b = $this->validate();
+		if(!$b){
+			$this->sendHmacError();
+		}
+		return $b;
+	}
+
+	private function sendHmacError(){
+		$ts = $this->parseTimestamp($this->ts_str);
+		$error = 'Erroe sconosciuto';
+		$code = ResponseCode::BAD_REQUEST;
+		if(!$this->user || !$this->ts_str || !$this->hash){
+			$error = "Message authentication failed: Wrong message format !";
+			$error .= " ( ts = $this->ts_str";
+			$error .= "  user = $this->user";
+			$error .= " hash = $this->hash )";
+			$code = ResponseCode::BAD_REQUEST;
+		}else if(!$ts){
+			$error = "Message authentication failed: Wrong timestamp format!";
+			$code = ResponseCode::BAD_REQUEST;
+		}else if(!self::isTimeStampValid($ts)){
+			$error = "Message authentication failed: Time offeset is too big !";
+			$code = ResponseCode::UNAUTHORIZED;
+		}else{
+			$api_key = $this->getApikey($this->user);
+			// $new_api_key = \Application\Utils\ApiKeyUtils::create();
+			$expected_hash = $this->calcHash();
+
+			if(strlen($expected_hash)>0 && ($expected_hash != $this->hash)){
+				$error = "Message authentication failed: Wrong hash !";
+				$code = ResponseCode::UNAUTHORIZED;
+			}
+		}
+		$this->responseStatus($code, array(), $error);
+
+	}
+
+	protected function calcHash(){
+		$hash = null;
+		$request = $this->app->request;
+		if($this->user && $this->ts_str){
+			$api_key = $this->getApikey($this->user);
+			if($api_key){
+				$data = $this->user . $this->ts_str . $api_key;
+				$raw_hash = hash_hmac('sha256', $data, $api_key, true); // Vedi https://en.wikipedia.org/wiki/Hash-based_message_authentication_code
+				$hash = base64_encode($raw_hash);
+			}
+		}
+		return $hash;
+	}
+
+	protected function validate(){
+
+		// From the comments in the PHP manual page (php.net/manual/en/function.urlencode.php) :
+		// "Don't use urlencode() or urldecode() if the text includes an email address,
+		// as it destroys the "+" character, a perfectly valid email address character.
+		// Unless you're certain that you won't be encoding email addresses
+		// AND you need the readability provided by the non-standard "+" usage,
+		// instead always use use rawurlencode() or rawurldecode().
+
+		if(!$this->user || !$this->ts_str || !$this->hash){
+			return false;
+		}
+
+		$api_key = $this->getApikey($this->user);
+		if(!$api_key){
+			return false;
+		}
+
+		$ts = $this->parseTimestamp($this->ts_str);
+		if(!$ts || !self::isTimeStampValid($ts)){
+			return false;
+		}
+
+		$expected = $this->calcHash();
+		if(!$expected){
+			return false;
+		}
+
+		if(Encryption::hashEquals($expected, $this->hash)){ // hash_equals($expected, $this->hash); // Solo per PHP > 5.6
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private function parseTimestamp($ts_str){
+		$ts = null;
+		if($ts_str){
+			try{
+				$ts = new \DateTime($ts_str);
+			} catch (\Exception $e) {
+				// $msg = $e->getMessage();
+			}
+		}
+		return $ts;
+	}
+
+
+
+	protected static function isTimeStampValid(\DateTime $dateTime){ // here default time-window is 20 minutes (1200 seconds)
+		// Note: a common issue when using time-windows for request,
+		// is that either server or client is not using the correct time.
+		// Make sure both server and client are using systems like NTP to keep times in sync
+
+		$b = false;
+		if($dateTime){
+			$now = new \DateTime();
+
+			// Normalizzo le timezone dei due timestamp
+			$dtz = new \DateTimeZone('Europe/Rome');
+			$now->setTimezone($dtz);
+			$dateTime->setTimezone($dtz);
+			$diffInSeconds = $now->getTimestamp() - $dateTime->getTimestamp();
+			$lifetime = $this->getLifetime();
+			$max_clockoffset = 600; // 10 minuti
+			if($dateTime && $diffInSeconds>-$max_clockoffset && $diffInSeconds<$lifetime){
+				$b = true; // vale true se l'hash è stato calcolato in un intervallo tra -10 minuti e +20 minuti
+			}
+		}
+		return $b;
+	}
+
+	protected function getLifetime(){
+		return 1200; // 20 minuti
+	}
+
+	private static function getTimeStampString(){
+		$now = new \DateTime();
+		// Nota: il formato data RFC3339 presenta il segno "+" nella descrizione della tiemzone
+		// per tale motivo non utilizzo urlencode() / urldecode()
+		$str = $now->format(\DateTime::RFC3339);
+		return $str;
+	}
+
+
+}
